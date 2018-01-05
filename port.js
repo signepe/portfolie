f8304763c837278f1dabc729bae57a328a2ce4c6
Signe Pedersen <signe_pe@Signes-MacBook-Pro-2.local>
2018-01-05 11:26:18 +0100

    portfolie

---
+++ port.js
@@ -0,0 +1,18 @@
+//$(window).on("load", startKnap);
+//
+//function startKnap() {
+//
+//
+//    $(".img").hide();
+//    $(".content").hide();
+//
+//    $(".start_btn").on("click", startBtnHandler);
+//
+//}
+//
+//function startBtnHandler() {
+//    console.log("Start billede + tekst");
+//
+//    $(".img").show();
+//    $(".content").show();
+//}
